2d5a25f5abc4dbe0494e4ec9e83a6fb363a9e823
jlandis320 <jreynoldslandis@gmail.com>
2022-08-31 17:09:15 -0400

    completes button eventListener func

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ const input = document.getElementById('input')
 const button = document.querySelector('#submit-button')
 const uList = document.querySelector('#todo-list')
 
-button.addEventListener('click', function(evt){
+button.addEventListener('click', function(event){
   // console.log("clicked");
   // create li element
   const li = document.createElement('li')
   // console.log(li);
   // set the text of the li element to the input text
   li.textContent = input.value
-  console.dir(li);
-  // reset the input text to empty string
+  console.log(li);
   // add the li element to the page, inside the ul element
-
+  document.querySelector('ul').appendChild(li)
+  // reset the input text to empty string
+  input.value = ""
 })
+
