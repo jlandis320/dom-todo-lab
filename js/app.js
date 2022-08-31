cf950411684b1a165e9329a77db125fb0b07851b
jlandis320 <jreynoldslandis@gmail.com>
2022-08-31 17:37:39 -0400

    changes input back to white

--- js/app.js
+++ js/app.js
@@ -6,13 +6,14 @@ button.addEventListener('click', function(event){
   // console.log("clicked");
   // create li element
   if (input.value === '') {
-    null
+    input.style.backgroundColor = 'salmon'
+    
   } else {
     const li = document.createElement('li')
     // console.log(li);
     // set the text of the li element to the input text
     li.textContent = input.value
-    console.log(li);
+    // console.log(li);
     // add the li element to the page, inside the ul element
     document.querySelector('ul').appendChild(li)
     // reset the input text to empty string
